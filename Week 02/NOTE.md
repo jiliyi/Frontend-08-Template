2d2cafebc0a5209ffc6b6dd7d516c33fecc74e83
jiali yin <2528966877@qq.com>
2021-01-03 12:12:57 +0800

    week02

--- Week 02/NOTE.md
+++ Week 02/NOTE.md
@@ -1,1 +1,34 @@
 学习笔记
+
+## 第一课 地图编辑器实现
+- 功能：在100 * 100的地图里，点击鼠标左键可以在地图对应处加障碍，右键可以消除障碍物。并可以保存地图
+- 实现 ：
+    1用一维数组表示地图( 长度：100 * 100),数组中0表示该处无障碍物，1表示有障碍物。
+    2双层for循环，生成地图元素(cell)
+    3给cell添加事件监听函数(mousemove)，添加事件之前，给几个全局标识，mousedown(鼠标是否按下,默认false)，clear(是否要清除,默认为false)。取消鼠标右键默认事件，具体js代码如下：
+```js
+    //设置标识
+let mousedown = false;
+let clear = false
+document.addEventListener('mousedown',()=>mousedown = true)
+
+//阻止鼠标右键默认事件
+document.addEventListener('contextmenu',e=>e.preventDefault())
+document.addEventListener('mousemove',(e)=>{
+    //e.which 左1  右3
+    clear = e.which === 3
+})
+
+document.addEventListener('mouseup',()=>mousedown = false)
+```
+   4有了上面的代码，再给cell添加事件监听函数，先判断是否在移动，然后判断点的是左键还是右键。左键添加障碍，右键清除障碍。
+   5保存地图的逻辑，在localStorage存一份就行了。
+
+## 第二课笔记
+- 实现广度优先搜索（以该案例地图为例）
+- 概述：给定起点和终点坐标，找到起点到终点的路径。
+- 思路：从起点开始，取到起点上下左右的点，_这些点中如果能走通，加入到能走点的集合中，再继续找能走点四周的点，依次类推。_为了防止重复走，走过的点加标识，下次遇到直接跳过。遇到边界的墙就不加入。此时可能想到用递归，但是递归适用于深度优先搜索，因为递归的话就会先找1周围的点，然后在1周围的点中继续找。接着选用数组来表示集合，入栈出栈的操作就用数组提供的api(shift,push),具体代码见小项目。
+
+## 第四课笔记
+- 解决路径问题
+- 思路：因为每次insert的时候，
---
+++ Week 02/地图/js/index.js
@@ -0,0 +1,132 @@
+let container = document.querySelector('.mapContainer')
+let btn  = document.getElementById('btn')
+//利用一维数组来映射地图，约定0没有路障，1有路障
+let mapArr = localStorage['mapArr'] ? JSON.parse(localStorage['mapArr']) : new Array(100 * 100).fill(0);
+//根据数组渲染页面,i表示纵向，j表示横向
+function render(){
+    for(let i = 0 ; i < 100;i++){
+        for(let j = 0 ; j < 100;j++){
+             let cellClass = 
+              mapArr[i * 100 + j] == 1 ? "cell block" :'cell' 
+            let cell = document.createElement('div');
+            cell.className = cellClass;
+            cell.addEventListener('mousemove',function(){
+                if(mousedown){
+                    if(clear){
+                        //右键
+                        cell.className = 'cell';
+                        mapArr[i * 100 + j] = 0
+                    }else{
+                        cell.className = 'cell block'
+                        mapArr[i * 100 + j] = 1
+                    }
+                }
+            })
+            container.appendChild(cell)  
+        }
+    }
+}
+render()
+
+
+class Sorted{
+    constructor(arr,compare){
+        this.arr = arr.slice();
+        this.compare = compare || ((a,b)=> a-b);
+    }
+
+    //每次都取这里面最小的数值
+    take(){
+        if(this.arr.length === 0){//数组为空
+            return
+        }
+        //默认最小值第一项
+        let min = this.arr[0];
+        //最小值索引
+        let minIndex = 0;
+        for(let i = 0 ; i < this.arr.length;i++){//找到最小值及其索引
+            if(this.compare(min,this.arr[i]) > 0){
+                min = this.arr[i];
+                minIndex = i;
+            }
+        }
+        this.arr[minIndex] = this.arr[this.arr.length - 1];//将数组最后一位换位置
+        this.arr.pop()
+        return min;
+    }
+    give(item){
+        this.arr.push(item)
+    }
+}
+
+//设置标识
+let mousedown = false;
+let clear = false
+document.addEventListener('mousedown',()=>mousedown = true)
+
+//阻止鼠标右键默认事件
+document.addEventListener('contextmenu',e=>e.preventDefault())
+document.addEventListener('mousemove',(e)=>{
+    //e.which 左1  右3
+    clear = e.which === 3
+})
+
+document.addEventListener('mouseup',()=>mousedown = false)
+btn.addEventListener('click',()=>localStorage['mapArr'] = JSON.stringify(mapArr))
+
+//一个延时的函数
+function delay(durtion){
+    return new Promise(resolve=>{
+        setTimeout(resolve,durtion)
+    })
+}
+
+//实现广度优先搜索,给定起点，终点，找到路径
+async function findPath(arr,start,end){
+    // debugger;
+    function distance(point){
+        return (point[0] - end[0]) ** 2 + (point[1] - end[1]) ** 2
+    }
+    let queue = new Sorted([start],(a,b)=>distance(a) - distance(b));
+    let table = Object.create(arr) //创建一个地图坐标的副本，记录找到该路径的上一个点的坐标
+    async function insert(x,y,prev){
+        if(x <0 || x >= 100 || y < 0 || y >= 100){//边界直接终止
+            return
+        }
+        if(table[y * 100 + x]){//遇到障碍直接跳过，已加入过队列的，不再重复加入。
+            return;
+        }
+        container.children[y * 100 + x].style.backgroundColor = 'lightgreen'
+        queue.give([x,y])
+        table[y * 100 + x] =  prev;
+        arr[y * 100 + x] = 2;
+    }
+        while(queue.arr.length){
+        let [x,y] = queue.take()
+        container.children[y * 100 + x].style.backgroundColor = 'lightblue'
+        if(x === end[0] && y === end[1]){//到达目标点
+            let path = []//保存路径
+            while(x !== start[0] || y !== start[1]){
+                path.push(table[y * 100 + x])
+                await delay(30);
+             [x,y] = table[y * 100 + x];
+ 
+            container.children[y * 100 + x].style.backgroundColor = 'purple'
+            }
+            return path;
+        }
+        //四周
+        
+       await insert(x-1,y,[x,y])
+       await insert(x + 1,y,[x,y])
+       await insert(x,y-1,[x,y])
+       await insert(x,y+1,[x,y])
+
+       //斜向
+       await insert(x-1,y-1,[x,y])
+       await insert(x+1,y+1,[x,y])
+       await insert(x-1,y+1,[x,y])
+       await insert(x+1,y-1,[x,y])
+    }  
+    return null  
+}
