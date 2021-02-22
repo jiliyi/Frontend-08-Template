432778eb4db3e9147d94d7be1bd138b76fa14642
jiali yin <2528966877@qq.com>
2021-02-22 20:59:06 +0800

    a

--- Week 08/服务器/server.js
+++ Week 08/服务器/server.js
@@ -1,8 +1,19 @@
 const http = require('http')
 
 http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type': 'text/html'})
-    res.write('hello')
-    res.end()
-}).listen(9527)
-console.log('server listen on 9527')
+    let body = [];
+    req.on('error',(err)=>{
+        console.log(err)
+    })
+    req.on('data',chunk=>{
+        body.push(chunk.toString())
+    })
+    req.on('end',()=>{
+        body = Buffer.concat(body).toString()
+        console.log('body',body)
+        res.writeHead(200,{'Content-Type': 'text/html'})
+        res.end('hello world')
+    })
+    
+}).listen(8088)
+console.log('server listen on 8088')
